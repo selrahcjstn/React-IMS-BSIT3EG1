772979f09661afaff50e48a9ac23b1657c9dcc87
selrahcjstn <charlesjustinemantes.main@gmail.com>
2025-11-07 11:17:46 +0800

    Refactor AuthProvider to manage user state and loading status with improved context integration

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,13 +1,31 @@
-import { useState } from "react";
-import AuthContext from "./AuthContext.jsx";
+import { useState, useEffect } from "react";
+import AuthContext from "./AuthContext"; 
+import {auth} from "../firebase/config";
+import { onAuthStateChanged } from "firebase/auth";
 
 export function AuthProvider({ children }) {
-  const [uid, setUid] = useState("");
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
 
   const value = {
-    uid,
-    setUid,
+    currentUser: currentUser,
+    uid: currentUser ? currentUser.uid : null,
+    email: currentUser ? currentUser.email : null,
+    isLoggedIn: !!currentUser,
   };
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 }
